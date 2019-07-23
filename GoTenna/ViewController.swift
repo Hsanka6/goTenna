a5ef97ab89b40095a1d59160182db9976354ec87
Hsanka6 <haasith2@gmail.com>
2019-07-23 00:47:43 -0700

    Setup mapbox

---
+++ GoTenna/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoTenna
+//
+//  Created by Haasith Sanka on 7/23/19.
+//  Copyright © 2019 Haasith Sanka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-GoTennaTests/Pods-GoTennaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoTennaTests : NSObject
+@end
+@implementation PodsDummy_Pods_GoTennaTests
+@end
